1a27e6fc52ef08cb1120475cd7a1d2ac23b25d0b
MohammadNs25 <m_n83586@live.concordia.ca>
2025-11-02 18:56:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,3 +24,13 @@ from timm.data.constants import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 import natten
 from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
 from sklearn.preprocessing import label_binarize
+
+
+def download_checkpoint(url, path):
+    print(f"Downloading checkpoint from {url}...")
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(path, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                        f.write(chunk)
+    print(f"Checkpoint downloaded and saved to {path}")
