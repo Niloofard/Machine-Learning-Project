fce6ad0da785c49d9910a097904905da46993206
Sajad Ahmadian Fini <sajadahmadian.ee@yahoo.com>
2025-11-07 21:54:39 -0500

    Define the MNIST training routine

--- main.py
+++ main.py
@@ -25,7 +25,6 @@ import natten
 from sklearn.metrics import precision_score, recall_score, f1_score, roc_auc_score, confusion_matrix
 from sklearn.preprocessing import label_binarize
 
-#test
 def download_checkpoint(url, path):
     print(f"Downloading checkpoint from {url}...")
     with requests.get(url, stream=True) as r:
@@ -34,6 +33,75 @@ def download_checkpoint(url, path):
             for chunk in r.iter_content(chunk_size=8192):
                         f.write(chunk)
     print(f"Checkpoint downloaded and saved to {path}")
+
+
+# Define the MNIST training routine
+def train_mnist(epochs, net, train_loader, test_loader, optimizer, scheduler, loss_function, device, save_path,
+                data_flag, task):
+    best_acc = 0.0
+    for epoch in range(epochs):
+        net.train()
+        running_loss = 0.0
+        train_bar = tqdm(train_loader, file=sys.stdout)
+        for step, datax in enumerate(train_bar):
+            images, labels = datax
+            images, labels = images.to(device), labels.to(device)
+            optimizer.zero_grad()
+            outputs = net(images)
+
+            if task == 'multi-label, binary-class':
+                labels = labels.to(torch.float32)
+                loss = loss_function(outputs, labels)
+            else:
+                labels = labels.squeeze().long()
+                loss = loss_function(outputs.squeeze(0), labels)
+
+            loss.backward()
+            optimizer.step()
+            scheduler.step()
+            running_loss += loss.item()
+
+            train_bar.desc = f"train epoch[{epoch + 1}/{epochs}] loss:{loss:.3f}"
+
+        net.eval()
+        y_score = torch.tensor([])
+        with torch.no_grad():
+            val_bar = tqdm(test_loader, file=sys.stdout)
+            for val_data in val_bar:
+                inputs, targets = val_data
+                outputs = net(inputs.to(device))
+
+                if task == 'multi-label, binary-class':
+                    targets = targets.to(torch.float32)
+                    outputs = outputs.softmax(dim=-1)
+                else:
+                    targets = targets.squeeze().long()
+                    outputs = outputs.softmax(dim=-1)
+                    targets = targets.float().resize_(len(targets), 1)
+
+                y_score = torch.cat((y_score, outputs.cpu()), 0)
+
+        y_score = y_score.detach().numpy()
+        evaluator = Evaluator(data_flag, 'test', size=224, root='./data')
+        metrics = evaluator.evaluate(y_score)
+
+        val_accurate, _ = metrics
+        print(
+            f'[epoch {epoch + 1}] train_loss: {running_loss / len(train_loader):.3f}  auc: {metrics[0]:.3f}  acc: {metrics[1]:.3f}')
+        print(f'lr: {scheduler.get_last_lr()[-1]:.8f}')
+        if val_accurate > best_acc:
+            print('\nSaving checkpoint...')
+            best_acc = val_accurate
+            state = {
+                'model': net.state_dict(),
+                'optimizer': optimizer.state_dict(),
+                'lr_scheduler': scheduler.state_dict(),
+                'acc': best_acc,
+                'epoch': epoch,
+            }
+            torch.save(state, save_path)
+
+    print('Finished Training')
 
 # Define the non-MNIST training routine
 def specificity_per_class(conf_matrix):
